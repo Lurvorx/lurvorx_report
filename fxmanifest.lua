00740ced55689b794ed42a184b24457b495cde6b
Lurvorx <88720533+Lurvorx@users.noreply.github.com>
2024-06-21 00:03:14 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "Lurvorx Scripts"
+description "A basic reportsystem made by Lurvorx Scripts"
+
+server_script {
+    "server/server.lua",
+    "config.lua"
+}
+
+shared_script '@es_extended/imports.lua'
